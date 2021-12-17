e7496897d5633e678698398c203683cce359801a
root <sendmej@outlook.com>
2021-12-16 21:11:04 -0800

    updated dockerout and kubernetesout

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=josephjoy038/pyapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run pyapp\
+    --image=$dockerpath\
+    --port=80 --labels app=pyapp
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pyapp 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=josephjoy038/pyapp
 
 # Step 2:  
 # Authenticate & tag
+docker login -u josephjoy038 -p 37c002e6-e89f-48ba-a5aa-bf5c6157fce6
+docker image tag app $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
